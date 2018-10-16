79fb4d063c015edbff8f3f361b2c0b5f43de8e05
cyh <j38472@outlook.com>
2018-10-16 11:13:49 +0800

    目标确定：腾讯防水墙可疑用户的极验

---
+++ geetest/src/main/java/TWWall/GeettestCrawler.java
@@ -0,0 +1,13 @@
+package TWWall;
+
+/**
+ * 破解腾讯防水墙可疑用户的极验
+ * @author  cyh
+ * 参考项目：https://www.cnblogs.com/w-y-c-m/p/7359455.html
+ */
+public class GeettestCrawler {
+    //验证码所在
+    public String INDEX_URL = "https://007.qq.com/online.html?ADTAG=capt.slide";
+
+
+}
