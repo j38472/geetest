2580b9ecaefaf96928b00cdbd45c3f90c894bab4
cyh <j38472@outlook.com>
2018-10-17 13:58:27 +0800

    获取原始图方法与获取背景图的方法

--- geetest/src/main/java/BiLi/GeettestCrawler.java
+++ geetest/src/main/java/BiLi/GeettestCrawler.java
@@ -1,11 +1,22 @@
 package BiLi;
 
+import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /**
  * bilibili极验验证码
@@ -17,11 +28,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  * 2.获取图片
  * 3.拼凑出图片
  * 4.计算需要平移的距离
- * 5.平移过去（尽可能模拟人的先快后慢的轨迹习惯）
+ * 5.平移过去（尽可能模拟人的先快后慢的轨迹习惯）--------!----!--!-!-(将平移时间平均分配到这五段上)可以再加上随机数
  */
 public class GeettestCrawler {
-    //验证码URL
-    private static String INDEX_URL = "https://passport.bilibili.com/login";
+    private static String basePath = "src/main/resources";
+    private static String FULL_IMAGE_NAME = "full-image";//完整的图像名称
+    private static String BG_IMAGE_NAME = "bg-image";   //背景图像名称
+    private static String INDEX_URL = "https://passport.bilibili.com/login";//验证码URL
+    private static int [][] moveArray ;// = new int [52][2];
+    private static boolean moveArrayInit = false;
     private static int TestTimes = 1000;//测试次数
     private static int successTimes = 0;//成功次数
     private static WebDriver driver;//selenium 实例声明
@@ -37,7 +52,11 @@ public class GeettestCrawler {
 
     public static void main(String[] args) {
         for (int i = 0; i < TestTimes; i++) {//测试次数
-            invoke();
+            try {
+                invoke();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             System.out.println("当前在测第" + i + 1 + "次,已成功" + successTimes + "次,成功率:" + ((double) successTimes / (double) i + 1) * 100 + "%");
 
         }
@@ -48,14 +67,14 @@ public class GeettestCrawler {
     /**
      * 逻辑调用层
      */
-    public static void invoke() {
+    public static void invoke() throws IOException {
         driver.get(INDEX_URL);
 
         By moveBet = By.cssSelector(".gt_slider_knob.gt_show");//滑动验证码滑动按钮
         waitForLoad(driver, moveBet);//等待十秒
         WebElement moveElemet = driver.findElement(moveBet);//获取验证码滑动按钮
-        int i = 0 ;
-        while (i++ <15){
+        int i = 0;
+        while (i++ < 15) {
             int distance = getMoveDistance(driver);
         }
     }
@@ -68,9 +87,80 @@ public class GeettestCrawler {
     }
 
     /**
+     * 计算需要平移的距离
+     *
+     * @param driver 需要计算的模块
+     * @return 平移距离
+     */
+    public static int getMoveDistance(WebDriver driver) throws IOException {
+        String pageSource = driver.getPageSource();//获取页面源码
+        String fullImageUrl = getFullImageUrl(pageSource);//获取原始图片Url
+        FileUtils.copyURLToFile(new URL(fullImageUrl), new File(basePath + FULL_IMAGE_NAME + ".jpg"));
+        String getBgImageUrl = getBgimageUrl(pageSource);
+        FileUtils.copyURLToFile(new URL(getBgImageUrl), new File(basePath + BG_IMAGE_NAME + ".jpg"));
+
+        return 1;
+    }
+
+    /**
+     * 获取move数组
+     * @param driver
+     */
+    private static void initMoveArray(WebDriver driver){
+        if (moveArrayInit){
+
+        }
+    }
+
+    /**
+     * 获取原始图URL
+     *
+     * @param pageSource 源码
+     * @return 原始图URL
+     */
+    private static String getFullImageUrl(String pageSource) {
+        String url = null;
+        Document document = Jsoup.parse(pageSource);
+        // 元素中第一个元素(first()) 返回属性值(attr())　
+        String style = document.select("[class=gt_cut_fullbg_slice]").first().attr("style");
+        //获取碎片图片的URL的编译好的正则表达式
+        Pattern pattern = Pattern.compile("url\\(\"(.*)\"\\)");
+        //将编译好的正则表达式放入正则适配器中
+        Matcher matcher = pattern.matcher(style);//
+        if (matcher.find()) {
+            url = matcher.group(1);
+        }
+        url = url.replace(".webp", ".jpg");
+        System.out.println(url);
+        return url;
+    }
+
+    /**
+     * 获取带背景的URL
+     *
+     * @param pageSoutrce 源码
+     * @return 背景图的URL
+     */
+    public static String getBgimageUrl(String pageSoutrce) {
+        String url = null;
+        Document document = Jsoup.parse(pageSoutrce);
+        String style = document.select(".gt_cut_bg_slice").first().attr("style");
+        Pattern pattern = Pattern.compile("url\\(\"(.*)\"\\)");
+        Matcher matcher = pattern.matcher(style);
+        if (matcher.find()) {
+            url = matcher.group(1);
+        }
+        url = url.replace(".webp", ".jpg");
+        System.out.println(url);
+        return url;
+    }
+
+
+    /**
      * selenium的显示等待(硬性要求等待时间达到指定时间 显示等待不需要) 等待元素加载 10S
+     *
      * @param driver 浏览器模拟器
-     * @param by 需要等待加载的元素
+     * @param by     需要等待加载的元素
      */
     public static void waitForLoad(final WebDriver driver, final By by) {
         new WebDriverWait(driver, 10).until(new ExpectedCondition<Boolean>() {
@@ -82,17 +172,6 @@ public class GeettestCrawler {
                 return false;
             }
         });
-    }
-
-    /**
-     * 计算需要平移的距离
-     * @param driver 需要计算的模块
-     * @return 平移距离
-     */
-    public static int getMoveDistance(WebDriver driver) {
-
-
-        return 1;
     }
 
 
