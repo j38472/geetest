8d67ad31f7e5ae264d9382796b66d399ebbf206d
cyh <j38472@outlook.com>
2018-10-17 20:08:05 +0800

    图片拼接 生成新图片

--- geetest/src/main/java/BiLi/GeettestCrawler.java
+++ geetest/src/main/java/BiLi/GeettestCrawler.java
@@ -5,33 +5,26 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.*;
-import org.openqa.selenium.Point;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.annotation.processing.SupportedSourceVersion;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReadParam;
 import javax.imageio.ImageReader;
 import javax.imageio.stream.ImageInputStream;
 import java.awt.*;
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Queue;
-import java.util.Random;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 
 /**
  * bilibili极验验证码
@@ -130,12 +123,81 @@ public class GeettestCrawler {
             cutPic(basePath + type + ".jpg", basePath + "result/" + type + ".jpg", -moveArray[i][0], -moveArray[i][1], 10, 58);
 
         }
+        //拼接图片
+        String[] b = new String[(int) pieceNummber / 2];
 
 
     }
 
     /**
+     * 图片拼接
+     *
+     * @param files      需要拼接的文件列表
+     * @param type       1横向拼接 2纵向拼接
+     * @param targetFile 输出文件
+     */
+    private static void mergeImage(String[] files, int type, String targetFile) {
+        int length = files.length;
+        File[] src = new File[length];
+        BufferedImage[] images = new BufferedImage[length];
+        int[][] ImageArrays = new int[length][];
+        for (int i = 0; i < length; i++) {
+            try {
+                src[i] = new File(files[i]);
+                images[i] = ImageIO.read(src[i]);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            int width = images[i].getWidth();
+            int height = images[i].getHeight();
+            ImageArrays[i] = new int[width * height];
+            ImageArrays[i] = images[i].getRGB(0, 0, width, height, ImageArrays[i], 0, width);
+        }
+        int newHeigth = 0;
+        int newWidth = 0;
+        for (int i = 0; i < images.length; i++) {
+            //横向
+            if (type==1){
+                newHeigth = newHeigth>images[i].getHeight()?newHeigth:images[i].getHeight();
+                newWidth += images[i].getWidth();
+            }else if(type == 2){
+                //纵向
+                newWidth = newWidth > images[i].getWidth()?newWidth:images[i].getWidth();
+                newHeigth += images[i].getHeight();
+            }
+        }
+        if (type == 1 && newWidth < 1){
+            return;
+        }
+        if (type == 2 && newHeigth <1){
+            return;
+        }
+        //生成新图片
+        try {
+
+            BufferedImage ImageNew = new BufferedImage(newWidth,newHeigth,BufferedImage.TYPE_INT_RGB);
+            int height_i = 0;
+            int width_i =0;
+            for (int i =0 ; i<images.length;i++){
+                if (type == 1){
+                    ImageNew.setRGB(width_i,0,images[i].getWidth(),newHeigth,ImageArrays[i],0,newWidth);
+                    height_i += images[i].getHeight();
+                }
+            }
+            //输出新图片
+            ImageIO.write(ImageNew, targetFile.split("\\.")[1], new File(targetFile));
+        }catch (Exception e){
+            System.err.println("生成新图片失败");
+            throw  new RuntimeException(e);
+        }
+
+
+    }
+
+
+    /**
      * 分割图片
+     *
      * @param srcFile 图片路径\\名称.jpg
      * @param outFile 图片输出路径//名称.jpg
      * @param x       图片坐标x
